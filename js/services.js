ed1e6a044254bd0a85c7a5448ee2e8932e5f6dc2
Dmitriy <dima_synyak@mail.ru>
2015-10-02 15:03:43 +0300

    02.10.215 - v1.0 Add files  services.js -  (warning)

--- js/services.js
+++ js/services.js
@@ -2,7 +2,11 @@ window.onload = function(){
     (function($){
             $('.order_btn').click(function(){
                 $('.tab').css('display', 'none');
+                $('#row1').css('display', 'block');
+                $('#row3').css('display', 'block');
                 $('#'+$(this).data('target')).slideDown(500);
+
+                $('html, body').animate({scrollTop: $('#row1').offset().top-100}, '500');
                 return false;
             });
     })(jQuery);
